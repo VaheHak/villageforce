f57b30f525c7b41b6799f1067e390da3ba1661b2
VaheHak <vahe.hakobyan.1997@mail.ru>
2021-04-07 22:24:08 +0400

    first commit

---
+++ controllers/CandidatesController.js
@@ -0,0 +1,15 @@
+
+class CandidatesController {
+  static list = async (req, res, next) => {
+    try {
+
+      res.json({
+
+      });
+    } catch (e) {
+      next(e);
+    }
+  }
+}
+
+export default CandidatesController;
---
+++ controllers/PeopleController.js
@@ -0,0 +1,15 @@
+
+class PeopleController {
+    static authorize = async (req, res, next) => {
+      try {
+
+        res.json({
+
+        });
+      } catch (e) {
+        next(e);
+      }
+    }
+}
+
+export default PeopleController;
